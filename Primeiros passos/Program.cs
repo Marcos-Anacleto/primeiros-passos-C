b5332dec03ca5b6aecf5364ac0f616084f846250
Marcos Anacleto <marcos_anacleto@outlook.com>
2021-02-26 20:53:08 -0300

    variaveis
    Primeiras variaveis

---
+++ Primeiros passos/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Primeiros_passos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            string nome = "";
+            Console.WriteLine("Escreva seu nome: ");
+            nome = Console.ReadLine();
+            Console.WriteLine("Seu nome é: ");
+            Console.WriteLine(nome);
+            */
+
+            /*
+            string nome = "";
+            Console.WriteLine("Escreva seu nome: ");
+            nome = Console.ReadLine();
+            Console.WriteLine("Seu nome é: ");
+            Console.WriteLine(nome);
+            */
+
+            string nome = "";
+            Console.WriteLine("Qual a soma de 20 + 20: ");
+            nome = Console.ReadLine();
+            Console.WriteLine("O resultado é: ");
+            Console.WriteLine(nome);
+
+            Console.ReadLine();  
+        }
+    }
+}
